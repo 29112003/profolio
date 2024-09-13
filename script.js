979cf833922841b4c1ccc45322eae77e649c4010
thisiskeshavyadavv@gmail.com <Keshav yadav>
2024-09-13 22:44:53 +0530

    bilding the span parent-child feature by javascript function

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-var loadinganimation =  function(){
-    var tl = gsap.timeline();
+// var loadinganimation =  function(){
+//     var tl = gsap.timeline();
 
-tl
-.to(".fullscreen",{
-    height:"0%",
-    duration:2,
-    delay:0.2,
-    ease:Expo.easeInOut,
-})
-.to(".middle-mover",{
-    height:"100%",
-    duration:2,
-    delay:-2,
-    ease:Expo.easeInOut,
-})
-.to(".page1",{
-    height:"100%",
-    duration:2,
-    delay:-1.7,
-    ease:Expo.easeInOut,
-})
+// tl
+// .to(".fullscreen",{
+//     height:"0%",
+//     duration:2,
+//     delay:0.2,
+//     ease:Expo.easeInOut,
+// })
+// .to(".middle-mover",{
+//     height:"100%",
+//     duration:2,
+//     delay:-2,
+//     ease:Expo.easeInOut,
+// })
+// .to(".page1",{
+//     height:"100%",
+//     duration:2,
+//     delay:-1.7,
+//     ease:Expo.easeInOut,
+// })
 
-}
+// }
 
-loadinganimation();
+// loadinganimation();
+
+document.querySelectorAll(".reveal").forEach(function(elem){
+    let parentSpan =  document.createElement("span")
+    let childSpan = document.createElement("span")
+    
+    parentSpan.classList.add("parent");
+    childSpan.classList.add("child");
+
+    childSpan.textContent = elem.textContent;
+
+    parentSpan.appendChild(childSpan)
+    elem.innerHTML = "";
+    elem.appendChild(parentSpan);
+
+})
